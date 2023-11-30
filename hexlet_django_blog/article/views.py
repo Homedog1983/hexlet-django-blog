ddae3a98d93f4bf0201772f5f1d5763dff455273
Evgeny Sokolov <sokolovsolod@gmail.com>
2023-11-30 21:13:00 +0700

    v 0.3.2 Add list of articles (CRUD) through class-based view

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from hexlet_django_blog.article.models import Article
 
 
 class IndexView(View):
@@ -7,11 +8,10 @@ class IndexView(View):
     template = 'articles/index.html'
 
     def get(self, request, *args, **kwargs):
-        return render(
-            request,
-            self.template,
-            context={'tags': self.tags},
-        )
+        articles = Article.objects.all()[:15]
+        return render(request, 'articles/index.html', context={
+            'articles': articles,
+        })
 
 
 class TagsIdView(View):
